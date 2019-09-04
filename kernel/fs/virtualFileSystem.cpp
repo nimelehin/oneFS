f98fa5f8f8cccb8f8813b0e90021ecf1f47b7085
nimelehin <nimelehin@gmail.com>
2019-09-04 18:49:21 +0300

    disk name assign

--- kernel/fs/virtualFileSystem.cpp
+++ kernel/fs/virtualFileSystem.cpp
@@ -7,13 +7,13 @@ VirtualFileSystem::VirtualFileSystem(): nextDiskNum(0) {}
 
 char VirtualFileSystem::attach(DiskDriver *driver) {
     disks[nextDiskNum++] = recognize(driver);
-    return 'A';
+    return disks[nextDiskNum-1]->diskName;
 }
 
 DiskDescriptor* VirtualFileSystem::recognize(DiskDriver *driver) {
     if (Fat16::testDisk(driver)) {
         Fat16 *fs = new Fat16(driver);
-        DiskDescriptor *desc = new DiskDescriptor('A', (uint64_t*)fs);
+        DiskDescriptor *desc = new DiskDescriptor(nextDiskNum+'A', (uint64_t*)fs);
         return desc;
     }
     return new DiskDescriptor();
