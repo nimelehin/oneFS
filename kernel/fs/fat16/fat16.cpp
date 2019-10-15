4c43d601f48a7e60754232f460c337f4e201dfc6
nimelehin <nimelehin@gmail.com>
2019-10-15 16:13:50 +0300

    bug fix & comments

--- kernel/fs/fat16/fat16.cpp
+++ kernel/fs/fat16/fat16.cpp
@@ -5,22 +5,25 @@ Fat16::Fat16(DiskDriver *t_disk): FileSystem(t_disk), mDirCache(){
     readParams();
 }
 
+// REQUIRED BY VFS
+// testDisk is supposed to test a disk and recognise if the disk 
+// has the file system which can be operated by this driver
 bool Fat16::testDisk(DiskDriver *t_disk) {
     t_disk->seek(0);
-    std::cout << t_disk << "\n";
-    
-    unsigned char* res = t_disk->readSector();
+    uint8_t* res = t_disk->readSector();
     char fat16Signature[] = {'F', 'A', 'T', '1', '6', 0x20, 0x20, 0x20};
-    bool sigCorrect = true;
+    bool isSignatureCorrect = true;
     for (uint16_t i = 0x36; i < 0x36 + 8; i++) {
-        sigCorrect &= (fat16Signature[i - 0x36] == res[i]);
+        isSignatureCorrect &= (fat16Signature[i - 0x36] == res[i]);
     }
-    return sigCorrect;
+    free(res);
+    return isSignatureCorrect;
 }
 
+// readParams is supposed to read all system data which need for FS
 void Fat16::readParams() {
     disk->seek(0);
-    unsigned char* data = disk->readSector();
+    uint8_t* data = disk->readSector();
     bytesPerSector = data[0x0c] * 0x100 + data[0x0b];
     sectorsPerCluster = 1; // data[0x0d]
     bytesPerCluster = bytesPerSector * sectorsPerCluster;
@@ -31,4 +34,5 @@ void Fat16::readParams() {
     rootEntries = data[0x12] * 0x100 + data[0x11];
     rootDirStart = bytesPerSector * reservedSectors + numberOfFATs * sectorsPerFAT * bytesPerSector; 
     dataSegStart = rootDirStart + rootEntries * 32;
+    free(data);
 }
